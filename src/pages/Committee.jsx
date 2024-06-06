2ae9f3e1654fa84426d1d9577278f592bd3e8ffc
Nitin Chakrawarti <nitinchakrawarti04@gmail.com>
2024-06-06 17:40:02 +0530

    executive committee 1

--- src/pages/Committee.jsx
+++ src/pages/Committee.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Particle from "../components/particle";
+import DrSanjeevSharma from "../components/ExecutiveComm";
+
 export default function Committee() {
   return (
     <>
       <Navbar />
       <Particle name="COMMITTEE" />
       <div>
-
-
         <div>
-
-          <h1 className="text-3xl text-center font-semibold">
+          <h1 className="text-4xl text-center font-semibold">
             {"Executive Committee".toUpperCase()}
           </h1>
 
           <div>{/* Dr.jitendra agarwal sir info   */}</div>
 
           <div>
-
+            <DrSanjeevSharma />
           </div>
-          
         </div>
 
         {/* In above section information about the Executive committee members is displayed */}
-
-
-
       </div>
     </>
   );
