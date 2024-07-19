334d39fe913a6bfb974c13af043b95cfb1e913cd
Abhishek Mishra <cs.abhishekmo@gmail.com>
2024-07-19 13:06:04 +0530

    added logo-rgpv on home page

--- src/components/HomeIntro.jsx
+++ src/components/HomeIntro.jsx
@@ -2,6 +2,8 @@ import { useState, useMemo, useEffect } from 'react'
 import Particles, { initParticlesEngine } from '@tsparticles/react'
 import { loadSlim } from '@tsparticles/slim'
 import { motion } from 'framer-motion'
+import rgpvLogo from "../assets/logo-rgpv.webp"
+
 
 export default function HomeIntro() {
 
@@ -84,7 +86,12 @@ export default function HomeIntro() {
                 animate={{ opacity: 1 }}
                 transition={{ duration: 2, ease: "easeInOut"}}
                 className="absolute text-white top-1/2 left-[10%] -translate-y-1/2  md:text-7xl sm:text-5xl text-2xl select-none">
-                <h2 className="sm:text-4xl md:text-6xl text-xl my-1 font-light">IEEE RGPV</h2>
+                <h2 className="sm:text-4xl md:text-6xl text-xl my-1 font-light">
+                    <div className="flex items-center gap-2">
+                    <img src={rgpvLogo} alt="logo-rgpv" className="md:h-[72px] h-[36px]" />
+                    IEEE RGPV
+                    </div>
+                </h2>
                 <h1 className="my-1">Institute of Electrical and</h1>
                 <h1 className="my-1">Electronics Engineers</h1>
             </motion.div>
