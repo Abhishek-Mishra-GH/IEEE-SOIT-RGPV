bcd9fc9e2e22a98b087ef1bfc81dc94507c9349c
harshadgajwaindia <harshadgajwaindia@gmail.com>
2024-08-08 16:27:12 +0530

    update

--- src/components/IEEEH.jsx
+++ src/components/IEEEH.jsx
@@ -15,7 +15,7 @@ export default function IEEEH() {
                     <div className="md:max-w-[45vw]  ">
                     <h3 className="text-3xl font-medium my-2">History of IEEE</h3>
                     <p className="text-xl">
-                    IEEE is the world's largest technical professional organisation dedicated to advancing innovation and technological excellence. Founded in 1884 with the establishment of the American Institute of Electrical Engineers (AIEE), it merged with the Institute of Radio Engineers (IRE) in 1963 to form IEEE. Originally focused on electrical and radio engineering, IEEE has grown to encompass a broad range of fields, including computing, bioengineering, robotics, and nanotechnology. 
+                    IEEE is the world's largest technical professional organization dedicated to advancing innovation and technological excellence. Founded in 1884 with the establishment of the American Institute of Electrical Engineers (AIEE), it merged with the Institute of Radio Engineers (IRE) in 1963 to form IEEE. Originally focused on electrical and radio engineering, IEEE has grown to encompass a broad range of fields, including computing, bioengineering, robotics, and nanotechnology. 
                         <br />
                         <br />
                         Today, IEEE supports over 395,000 members in 160 countries through its extensive network of societies, publications, and conferences, continually fostering global technological advancements.
