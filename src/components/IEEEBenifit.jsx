89f656528831aa0a4928331ce50ef6d8f01ce62c
harshadgajwaindia <harshadgajwaindia@gmail.com>
2024-08-09 15:00:48 +0530

    Update

--- src/components/IEEEBenifit.jsx
+++ src/components/IEEEBenifit.jsx
@@ -48,7 +48,7 @@ function Benefits() {
                 <div className="h-1 w-64 md:w-[20%] border bg-black my-1 mx-auto"></div>
             </h1>
 
-            <div className="-z-20 relative overflow-hidden w-full md:w-5/6 rounded-3xl  hover:bg-gray-100 md:left-32 ">
+            <div className="-z-0 relative overflow-hidden w-full md:w-5/6 rounded-3xl  hover:bg-gray-100 md:left-32 ">
                 <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
                     {benefits.map((benefit, index) => (
                         <div key={index} className="flex-none w-full flex flex-col md:flex-col items-center p-6 ">
@@ -68,13 +68,13 @@ function Benefits() {
 
                 <button
                     onClick= {handlePrev}
-                    className="-z-20 md:h-28 md:w-10 absolute top-1/2 left-2 transform -translate-y-1/2 text-black p-2 rounded-full bg-gray-300 hover:shadow-lg hover:shadow-gray-900 " 
+                    className="-z-0 md:h-28 md:w-10 absolute top-1/2 left-2 transform -translate-y-1/2 text-black p-2 rounded-full bg-gray-300 hover:shadow-lg hover:shadow-gray-900 " 
                 >
                     &lt;
                 </button>
                 <button
                     onClick={handleNext}
-                    className="-z-20 md:h-28 md:w-10 absolute top-1/2 right-2 transform -translate-y-1/2 text-black p-2 rounded-full bg-gray-300 hover:shadow-lg hover:shadow-gray-900 "
+                    className="-z-0 md:h-28 md:w-10 absolute top-1/2 right-2 transform -translate-y-1/2 text-black p-2 rounded-full bg-gray-300 hover:shadow-lg hover:shadow-gray-900 "
                 >
                     &gt;
                 </button>
